bb7207d67419d329d22d7435eedf6dfdbe207454
Olle Kaiser <yogsther@hotmail.com>
2014-02-05 17:03:49 +0100

    hela html skiten

--- index.php
+++ index.php
@@ -1,2 +1,25 @@
 <html>
- <head>
+
+<head>	<title> PrimaBook </title>
+
+</head>
+
+<body style="background-color:#c0392b;">
+
+
+
+<h1 style="background-color:#e74c3c;">Welcome to PrimaBook.TK </h1>  
+
+
+<button onclick="window.location.href='https://www.google.se/'">Home</button>
+<button onclick="window.location.href='https://www.google.se/'">My profile</button>
+<button onclick="window.location.href='https://www.google.se/'">i'm bored...</button>
+<button onclick="window.location.href='https://www.google.se/'">Log out</button>
+
+
+
+
+
+</body>
+
+</html>
