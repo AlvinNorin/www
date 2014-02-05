1d960ddb9197922360a9b5887d4ddeba6545bee9
Olle Kaiser <yogsther@hotmail.com>
2014-02-05 19:04:51 +0100

    test

--- index.php
+++ index.php
@@ -5,21 +5,24 @@
 </head>
 
 <body style="background-color:#c0392b;">
+<style>body { font-family: Ubuntu, sans-serif; } </style>
 
-
-
+<div>
 <h1 style="position: absolute; left: 0%; top: 0%; width: 100%; height: 5%; background-color:#e74c3c;">Welcome to PrimaBook.TK </h1>  
+</div>
 
+<div> 
+<h3 style="position: absolute; left: 85%; top: 5%; width: 15%; height: 15%; background-color:#e74c3c;"> Dagens Citat: "Example Text" </h3>
+</div>
 
-<button onclick="window.location.href='https://www.google.se/'">Home</button>
-<button onclick="window.location.href='https://www.google.se/'">My profile</button>
-<button onclick="window.location.href='https://www.google.se/'">i'm bored...</button>
-<button onclick="window.location.href='https://www.google.se/'">Log out</button>
-
-
-
+<!--   MENUBAR :)		-->
+<div style="position: absolute; top: 10%;">
+<button style="background:#e74c3c;border: none;" onclick="window.location.href='https://www.google.se/'">Home</button>
+<button style="background:#e74c3c;border: none;" onclick="window.location.href='https://www.youtube.com/'">My Profile</button>
+<button style="background:#e74c3c;border: none;" onclick="window.location.href='https://www.redtube.com/'">I'm Bored</button>
+<button style="background:#e74c3c;border: none;" onclick="window.location.href='https://www.facebook.com/'">Log out</button>
+</div>
 
 
 </body>
-
 </html>
